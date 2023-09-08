2dacb4aacc149c0bafb188e86dd4b5ef1dae9fda
Antoine Boucher <antoine.bou13@gmail.com>
2023-09-07 22:34:38 -0400

    small fix

--- RenphoWeight.py
+++ RenphoWeight.py
@@ -1,8 +1,8 @@
 import requests
 import json
-import time
 import datetime
-from threading import Timer
+from concurrent.futures import ThreadPoolExecutor
+import asyncio
 from Crypto.PublicKey import RSA
 from Crypto.Cipher import PKCS1_v1_5
 from base64 import b64encode
@@ -16,7 +16,6 @@ API_AUTH_URL = 'https://renpho.qnclouds.com/api/v3/users/sign_in.json?app_id=Ren
 API_SCALE_USERS_URL = 'https://renpho.qnclouds.com/api/v3/scale_users/list_scale_user'
 API_MEASUREMENTS_URL = 'https://renpho.qnclouds.com/api/v2/measurements/list.json'
 
-
 class RenphoWeight:
     """
     A class to interact with Renpho's weight scale API.
@@ -29,13 +28,11 @@ class RenphoWeight:
         weight (float): The most recent weight measurement.
         time_stamp (int): The timestamp of the most recent weight measurement.
         session_key (str): The session key obtained after successful authentication.
+        executor (ThreadPoolExecutor): Executor for running synchronous requests.
     """
 
     def __init__(self, public_key, email, password, user_id=None):
-        """
-        Initialize a new RenphoWeight instance.
-        """
-        _LOGGER.debug("Init RenphoWeight")
+        """Initialize a new RenphoWeight instance."""
         self.public_key = public_key
         self.email = email
         self.password = password
@@ -43,73 +40,61 @@ class RenphoWeight:
         self.weight = None
         self.time_stamp = None
         self.session_key = None  # Initialize session_key
+        self.executor = ThreadPoolExecutor(max_workers=4)
 
-    def _request(self, method, url, **kwargs):
+    async def _request(self, method, url, **kwargs):
         """
-        Make a generic API request and handle errors.
+        Asynchronous method to make an API request and handle errors.
         """
-        try:
-            response = requests.request(method, url, **kwargs)
-            response.raise_for_status()
-            return response.json()
-        except Exception as e:
-            _LOGGER.error(f"Error in request: {e}")
-            raise  # Or raise a custom exception
+        loop = asyncio.get_event_loop()
+        response = await loop.run_in_executor(
+            self.executor, requests.request, method, url, **kwargs
+        )
+        response.raise_for_status()
+        return response.json()
 
-    def auth(self):
+    async def auth(self):
         """
         Authenticate with the Renpho API to obtain a session key.
         """
+        if not self.email or not self.password:
+            raise Exception("Email and password must be provided")
 
-        if not self.email:
-            raise Exception("Email must be provided")
-
-        if not self.password:
-            raise Exception("Password must be provided")
-
-        # Encrypt the password using RSA encryption
         key = RSA.importKey(self.public_key)
         cipher = PKCS1_v1_5.new(key)
-        encrypted_password = b64encode(
-            cipher.encrypt(self.password.encode("utf-8")))
+        encrypted_password = b64encode(cipher.encrypt(self.password.encode("utf-8")))
 
-        # Make the authentication request
-        data = {'secure_flag': 1, 'email': self.email,
-                'password': encrypted_password}
-        parsed = self._request('POST', API_AUTH_URL, data=data)
+        data = {'secure_flag': 1, 'email': self.email, 'password': encrypted_password}
+        parsed = await self._request('POST', API_AUTH_URL, data=data)
 
         if 'terminal_user_session_key' not in parsed:
-            raise Exception("Authentication failed. Please check your username and password.")
+            raise Exception("Authentication failed.")
 
-
-        # Store the session key
         self.session_key = parsed['terminal_user_session_key']
         return parsed
 
     async def validate_credentials(self):
         """
         Validate the current credentials by attempting to authenticate.
-
-        Returns:
-            bool: True if authentication succeeds, False otherwise.
+        Returns True if authentication succeeds, False otherwise.
         """
         try:
-            self.auth()
+            await self.auth()
             return True
         except Exception as e:
             _LOGGER.error(f"Validation failed: {e}")
             return False
 
-    def getScaleUsers(self):
+    async def getScaleUsers(self):
         """
         Fetch the list of users associated with the scale.
         """
         url = f"{API_SCALE_USERS_URL}?locale=en&terminal_user_session_key={self.session_key}"
-        parsed = self._request('GET', url)
+        parsed = await self._request('GET', url)
         self.set_user_id(parsed['scale_users'][0]['user_id'])
         return parsed['scale_users']
 
-    def getMeasurements(self):
+    async def getMeasurements(self):
         """
         Fetch the most recent weight measurements for the user.
         """
@@ -117,37 +102,42 @@ class RenphoWeight:
         week_ago = today - datetime.timedelta(days=7)
         week_ago_timestamp = int(time.mktime(week_ago.timetuple()))
         url = f"{API_MEASUREMENTS_URL}?user_id={self.user_id}&last_at={week_ago_timestamp}&locale=en&app_id=Renpho&terminal_user_session_key={self.session_key}"
-        parsed = self._request('GET', url)
+        parsed = await self._request('GET', url)
         last_measurement = parsed['last_ary'][0]
         self.weight = last_measurement['weight']
         self.time_stamp = last_measurement['time_stamp']
         return parsed['last_ary']
 
-    def getSpecificMetric(self, metric):
-        """
-        Fetch a specific metric from the most recent weight measurement.
-        """
-        last_measurement = self.getMeasurements()[0]
-        # Return None if metric not found
-        return last_measurement.get(metric, None)
-
     def set_user_id(self, user_id):
         """
         Set the user ID for whom the weight data should be fetched.
-
-        Args:
-            user_id (str): The new user ID.
         """
         self.user_id = user_id
 
     def get_user_id(self):
         """
         Get the current user ID for whom the weight data is being fetched.
-
-        Returns:
-            str: The current user ID.
         """
         return self.user_id
+
+    def close(self):
+        """
+        Shutdown the executor when you are done using the RenphoWeight instance.
+        """
+        self.executor.shutdown()
+
+
+class Interval(Timer):
+    """
+    A subclass of Timer to repeatedly run a function at a specified interval.
+    """
+
+    def run(self):
+        """
+        Run the function at the given interval.
+        """
+        while not self.finished.wait(self.interval):
+            self.function(*self.args, **self.kwargs)
 
     def getSpecificMetricFromUserID(self, metric, user_id=None):
         """
--- __init__.py
+++ __init__.py
@@ -1,59 +1,57 @@
-"""Initialization for the Renpho sensor component."""
-
-# Import necessary modules and classes
-from .const import CONF_USER_ID, DOMAIN, CONF_EMAIL, CONF_PASSWORD, CONF_REFRESH, CONF_PUBLIC_KEY, EVENT_HOMEASSISTANT_START, EVENT_HOMEASSISTANT_STOP, EVENT_HOMEASSISTANT_CLOSE
+from homeassistant.helpers import service
+from .const import (
+    CONF_USER_ID, DOMAIN, CONF_EMAIL, CONF_PASSWORD,
+    CONF_REFRESH, CONF_PUBLIC_KEY,
+    EVENT_HOMEASSISTANT_START, EVENT_HOMEASSISTANT_STOP
+)
 from .RenphoWeight import RenphoWeight
-from .config_flow import RenphoConfigFlow
 import logging
-import sys
-import os
-sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
-logging.basicConfig(level=logging.DEBUG)
 
 # Initialize logger
 _LOGGER = logging.getLogger(__name__)
 
-
 def setup(hass, config):
-    """
-    Set up the Renpho component.
-
-    Args:
-        hass (HomeAssistant): Home Assistant core object.
-        config (dict): Configuration for the component.
-
-    Returns:
-        bool: True if initialization was successful, False otherwise.
-    """
-
-    _LOGGER.debug("Starting hass-renpho")
+    """Set up the Renpho component."""
+    _LOGGER.debug("Starting hass_renpho")
 
     # Extract configuration values
     conf = config[DOMAIN]
     email = conf[CONF_EMAIL]
     password = conf[CONF_PASSWORD]
-    user_id = conf[CONF_USER_ID]
+    user_id = conf.get(CONF_USER_ID)  # Using get in case it's optional
     refresh = conf[CONF_REFRESH]
 
     # Create an instance of RenphoWeight
     renpho = RenphoWeight(CONF_PUBLIC_KEY, email, password, user_id)
 
-    # Define a cleanup function to stop polling when Home Assistant stops
+    # Define a cleanup function
     def cleanup(event):
         renpho.stopPolling()
 
-    # Define a prepare function to start polling when Home Assistant starts
+    # Define a prepare function
     def prepare(event):
         renpho.startPolling(refresh)
         hass.bus.listen_once(EVENT_HOMEASSISTANT_STOP, cleanup)
 
-    # Register the prepare function to be called when Home Assistant starts
+    # Register the prepare function
     hass.bus.listen_once(EVENT_HOMEASSISTANT_START, prepare)
 
-    # Store the Renpho instance in Home Assistant's data dictionary
+    # Store the Renpho instance
     hass.data[DOMAIN] = renpho
 
-    return True  # Initialization was successful
+    # Reload configuration function
+    def reload_config(call):
+        """Reload the Renpho component."""
+        # Unload the current configuration
+        hass.helpers.entity_component.async_unload_entities(DOMAIN)
+
+        # Load the new configuration
+        setup(hass, config)
+
+    # Register the reload service
+    hass.services.register(DOMAIN, 'reload', reload_config)
+
+    return True
 
 if __name__ == "__main__":
     renpho = RenphoWeight(CONF_PUBLIC_KEY, '<username>', '<password>', '<user_id>')
