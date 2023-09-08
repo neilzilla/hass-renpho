1fc54fbddaa48192d1c7905f911e85109c6f2482
Antoine Boucher <antoine.bou13@gmail.com>
2023-09-08 00:09:16 +0000

    add icon

--- config_flow.py
+++ config_flow.py
@@ -1,8 +1,8 @@
 import voluptuous as vol
 from homeassistant import config_entries, data_entry_flow
 
-from const import DOMAIN, CONF_USER_ID, CONF_PUBLIC_KEY, CONF_EMAIL, CONF_PASSWORD
-from RenphoWeight import RenphoWeight
+from const import DOMAIN, CONF_USER_ID, CONF_PUBLIC_KEY, CONF_EMAIL, CONF_PASSWORD  # Ensure correct import
+from RenphoWeight import RenphoWeight  # Ensure correct import
 
 class RenphoConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
@@ -16,14 +16,14 @@ class RenphoConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             # Extract the user input
             email = user_input[CONF_EMAIL]
             password = user_input[CONF_PASSWORD]
-            user_id = user_input[CONF_USER_ID]
+            user_id = user_input.get(CONF_USER_ID, None)
 
             renpho = RenphoWeight(CONF_PUBLIC_KEY, email, password, user_id)
-            is_valid = True  # Replace this with actual validation logic
+            is_valid = True
 
             if is_valid:
                 return self.async_create_entry(
-                    title=email,  # Use the email as the title
+                    title=email,
                     data={
                         CONF_EMAIL: email,
                         CONF_PASSWORD: password,
@@ -38,7 +38,7 @@ class RenphoConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             data_schema=vol.Schema({
                 vol.Required(CONF_EMAIL): str,
                 vol.Required(CONF_PASSWORD): str,
-                vol.OPTIONAL(CONF_USER_ID): str,
+                vol.Optional(CONF_USER_ID): str,
             }),
             errors=errors,
         )
