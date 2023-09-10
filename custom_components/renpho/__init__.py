34a53ae94a51feb5ce034854f6d60bdd62912cd7
Antoine Boucher <antoine.bou13@gmail.com>
2023-09-10 14:31:16 -0400

    Update __init__.py

--- custom_components/renpho/__init__.py
+++ custom_components/renpho/__init__.py
@@ -1,49 +1,77 @@
+import logging
+import asyncio
 from homeassistant.helpers import service
-from .custom_components.renpho.const import (
+from homeassistant.core import callback
+
+from .const import (
     CONF_USER_ID, DOMAIN, CONF_EMAIL, CONF_PASSWORD,
     CONF_REFRESH, CONF_PUBLIC_KEY,
     EVENT_HOMEASSISTANT_START, EVENT_HOMEASSISTANT_STOP
 )
 from .RenphoWeight import RenphoWeight
-import logging
 
 # Initialize logger
 _LOGGER = logging.getLogger(__name__)
 
+# ------------------- Setup Methods -------------------
+
 async def async_setup(hass, config):
-    """Set up the Renpho component."""
+    """Set up the Renpho component from YAML configuration."""
     _LOGGER.debug("Starting hass_renpho")
-
-    # Extract configuration values
-    conf = config[DOMAIN]
-    email = conf[CONF_EMAIL]
-    password = conf[CONF_PASSWORD]
-    user_id = conf.get(CONF_USER_ID)  # Using get in case it's optional
-    refresh = conf[CONF_REFRESH]
-
-    # Create an instance of RenphoWeight
-    renpho = RenphoWeight(CONF_PUBLIC_KEY, email, password, user_id)
-
-    # Define a cleanup function
-    async def async_cleanup(event):
-        await renpho.stopPolling()
-
-    # Define a prepare function
-    async def async_prepare(event):
-        await renpho.startPolling(refresh)
-        hass.bus.async_listen_once(EVENT_HOMEASSISTANT_STOP, async_cleanup)
-
-    # Register the prepare function
-    hass.bus.async_listen_once(EVENT_HOMEASSISTANT_START, async_prepare)
-
-    # Store the Renpho instance
-    hass.data[DOMAIN] = renpho
-
+    
+    conf = config.get(DOMAIN)
+    if conf:
+        await setup_renpho(hass, conf)
     return True
 
-if __name__ == "__main__":
-    import asyncio
+async def async_setup_entry(hass, entry):
+    """Set up Renpho from a config entry."""
+    await setup_renpho(hass, entry.data)
 
+    hass.async_create_task(
+        hass.config_entries.async_forward_entry_setup(entry, "sensor")
+    )
+    
+    return True
+
+async def async_unload_entry(hass, entry):
+    """Unload a config entry."""
+    # Remove Renpho instance if it exists
+    await hass.config_entries.async_forward_entry_unload(entry, "sensor")
+    if DOMAIN in hass.data:
+        del hass.data[DOMAIN]
+        return True
+
+# ------------------- Helper Methods -------------------
+
+async def setup_renpho(hass, conf):
+    """Common setup logic for YAML and UI."""
+    email = conf[CONF_EMAIL]
+    password = conf[CONF_PASSWORD]
+    user_id = conf.get(CONF_USER_ID, None)
+    refresh = conf.get(CONF_REFRESH, 600)
+    
+    renpho = RenphoWeight(CONF_PUBLIC_KEY, email, password, user_id)
+
+    @callback
+    def async_on_start(event):
+        hass.async_create_task(async_prepare(hass, renpho, refresh))
+    
+    hass.bus.async_listen_once(EVENT_HOMEASSISTANT_START, async_on_start)
+    hass.data[DOMAIN] = renpho
+
+async def async_prepare(hass, renpho, refresh):
+    """Prepare and start polling."""
+    await renpho.startPolling(refresh)
+    hass.bus.async_listen_once(EVENT_HOMEASSISTANT_STOP, async_cleanup(renpho))
+
+async def async_cleanup(renpho):
+    """Cleanup logic."""
+    await renpho.stopPolling()
+
+# ------------------- Main Method for Testing -------------------
+
+if __name__ == "__main__":
     async def main():
         renpho = RenphoWeight(CONF_PUBLIC_KEY, '<username>', '<password>', '<user_id>')
         await renpho.startPolling(10)
