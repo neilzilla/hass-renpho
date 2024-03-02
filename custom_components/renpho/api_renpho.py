2e2a7e420be3a0d62e46d3070eb24fe499f2bf13
Antoine Boucher <antoine.bou13@gmail.com>
2024-03-02 14:15:03 -0500

    Update api_renpho.py

--- custom_components/renpho/api_renpho.py
+++ custom_components/renpho/api_renpho.py
@@ -62,6 +62,7 @@ class RenphoWeight:
         self.refresh = refresh
         self.session = aiohttp.ClientSession()
         self.is_polling_active = False
+        self.session_key_expiry = None
 
     def set_user_id(self, user_id):
         """
@@ -116,6 +117,10 @@ class RenphoWeight:
                     "No session key found. Attempting to authenticate.")
                 await self.auth()
 
+            if self.session_key is None or (self.session_key_expiry and self.session_key_expiry <= datetime.datetime.now()):
+                _LOGGER.warning("Session key has expired or is missing. Attempting to re-authenticate.")
+                await self.auth()
+
             # Prepare the data for the API request
             kwargs = self.prepare_data(kwargs)
 
@@ -125,18 +130,19 @@ class RenphoWeight:
                 parsed_response = await response.json()
 
                 # Handle specific status code 40302
-                if parsed_response.get("status_code") == "40302":
+                if parsed_response.get("status_code", 0) == 40302:
                     _LOGGER.warning(
                         "Received 40302 status code. Attempting to re-authenticate.")
+                    await self.auth()
 
                 return parsed_response
 
         except (aiohttp.ClientResponseError, aiohttp.ClientConnectionError) as e:
-            _LOGGER.error(f"Client error: {e}")
+            _LOGGER.error(f"Client error occurred in _request: {e}")
             raise APIError(f"API request failed {method} {url}") from e
 
         except Exception as e:
-            _LOGGER.error(f"Unexpected error: {e}")
+            _LOGGER.error(f"Unexpected error occurred in _request: {e}")
             raise APIError(f"API request failed {method} {url}") from e
 
     async def auth(self):
@@ -174,6 +180,7 @@ class RenphoWeight:
 
         # If everything is fine, set the session_key
         self.session_key = parsed["terminal_user_session_key"]
+        self.session_key_expiry = datetime.datetime.now() + datetime.timedelta(minutes=10)
         return parsed
 
     async def validate_credentials(self):
@@ -205,9 +212,9 @@ class RenphoWeight:
             self.set_user_id(parsed["scale_users"][0]["user_id"])
             return parsed["scale_users"]
         except aiohttp.ClientError as e:
-            _LOGGER.error(f"Aiohttp client error: {e}")
+            _LOGGER.error(f"Aiohttp client error in get_scale_users: {e}")
         except Exception as e:
-            _LOGGER.error(f"An unexpected error occurred: {e}")
+            _LOGGER.error(f"An unexpected error occurred in get_scale_users: {e}")
         return None
 
     async def get_measurements(self) -> Optional[List[Dict]]:
@@ -254,7 +261,7 @@ class RenphoWeight:
             await self.close()
             return None
         except Exception as e:
-            _LOGGER.error(f"An unexpected error occurred: {e} {url}")
+            _LOGGER.error(f"An unexpected error occurred in get_measurements: {e} {url}")
             await self.close()
             return None
 
@@ -318,7 +325,7 @@ class RenphoWeight:
             return last_measurement.get(metric, None) if last_measurement else None
 
         except Exception as e:
-            _LOGGER.error(f"An error occurred: {e} {metric_type} {metric}")
+            _LOGGER.error(f"An error occurred in get_specific_metric: {e} {metric_type} {metric}")
             await self.close()
             return None
 
@@ -349,27 +356,27 @@ class RenphoWeight:
         finally:
             await self.close()  # Close session
 
-
     def stop_polling(self):
         """
         Stop polling for weight data.
         """
-        if self.is_polling_active:
-            if hasattr(self, "polling"):
-                try:
-                    # Check if 'polling' is a Timer object or similar with a 'cancel' method
-                    if callable(getattr(self.polling, "cancel", None)):
-                        self.polling.cancel()
-                        _LOGGER.info("Successfully stopped polling.")
-                        self.is_polling_active = False
-                    else:
-                        _LOGGER.warning(
-                            "Attribute 'polling' exists but has no 'cancel' method.")
-                except Exception as e:
-                    _LOGGER.error(
-                        f"An error occurred while stopping polling: {e}")
-            else:
-                _LOGGER.warning("No active polling to stop.")
+        if not self.is_polling_active:
+            return
+        if hasattr(self, "polling"):
+            try:
+                # Check if 'polling' is a Timer object or similar with a 'cancel' method
+                if callable(getattr(self.polling, "cancel", None)):
+                    self.polling.cancel()
+                    _LOGGER.info("Successfully stopped polling.")
+                    self.is_polling_active = False
+                else:
+                    _LOGGER.warning(
+                        "Attribute 'polling' exists but has no 'cancel' method.")
+            except Exception as e:
+                _LOGGER.error(
+                    f"An error occurred while stopping polling: {e}")
+        else:
+            _LOGGER.warning("No active polling to stop.")
 
     async def get_device_info(self):
         """
@@ -439,7 +446,7 @@ class RenphoWeight:
             )
             return last_measurement.get(metric, None) if last_measurement else None
         except Exception as e:
-            _LOGGER.error(f"An error occurred: {e}")
+            _LOGGER.error(f"An error occurred in get_specific_girth_metric: {e}")
             return None
 
     async def list_girth_goal(self):
@@ -454,7 +461,7 @@ class RenphoWeight:
         try:
             return await self._request("GET", url)
         except Exception as e:
-            _LOGGER.error(f"An error occurred: {e}")
+            _LOGGER.error(f"An error occurred while listing girth goal: {e}")
             return None
 
     async def get_specific_girth_goal_metric(
@@ -479,20 +486,21 @@ class RenphoWeight:
             if not girth_goal_info or 'girth_goals' not in girth_goal_info:
                 return None
 
-            # Filter to find the specific metric
-            last_goal = next(
-                (goal for goal in girth_goal_info['girth_goals'] if goal['girth_type'] == metric),
-                None
-            )
-
-            if last_goal:
+            if last_goal := next(
+                (
+                    goal
+                    for goal in girth_goal_info['girth_goals']
+                    if goal['girth_type'] == metric
+                ),
+                None,
+            ):
                 return last_goal.get('goal_value', None)
 
             return None
 
         except Exception as e:
             await self.close()
-            print(f"An error occurred: {e}")
+            print(f"An error occurred in get_specific_girth_goal_metric: {e}")
             return None
 
     async def list_growth_record(self):
@@ -507,7 +515,7 @@ class RenphoWeight:
             return await self._request("GET", url)
         except Exception as e:
             await self.close()
-            print(f"An error occurred: {e}")
+            print(f"An error occurred in list_growth_record: {e}")
             return None
 
     async def get_specific_growth_metric(
@@ -535,9 +543,8 @@ class RenphoWeight:
             return last_measurement.get(metric, None) if last_measurement else None
         except Exception as e:
             await self.close()
-            print(f"An error occurred: {e}")
+            print(f"An error occurred in get_specific_growth_metric: {e}")
             return None
-
 
     async def message_list(self):
         """
@@ -549,7 +556,7 @@ class RenphoWeight:
             return await self._request("GET", url)
         except Exception as e:
             await self.close()
-            print(f"An error occurred: {e}")
+            print(f"An error occurred in message_list: {e}")
             return None
 
     async def request_user(self):
@@ -562,7 +569,7 @@ class RenphoWeight:
             return await self._request("GET", url)
         except Exception as e:
             await self.close()
-            print(f"An error occurred: {e}")
+            print(f"An error occurred in request_user: {e}")
             return None
 
     async def reach_goal(self):
@@ -575,7 +582,7 @@ class RenphoWeight:
             return await self._request("GET", url)
         except Exception as e:
             await self.close()
-            print(f"An error occurred: {e}")
+            print(f"An error occurred in reach_goal: {e}")
             return None
 
     async def close(self):
